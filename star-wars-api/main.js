20a8a3720ed5e382b7f0111b90a63d1ad691bcea
Thor Anderson <ta.anderson@gmail.com>
2020-10-20 12:37:21 -0600

    Looped through films and people

--- star-wars-api/main.js
+++ star-wars-api/main.js
@@ -1,3 +1,19 @@
 import { films } from './data/films.js'
+import { people } from './data/people.js'
 
-console.log(films[6])
+console.log(people.length)
+
+const main = document.querySelector('main')
+
+/* films.forEach(film => {
+    console.log(film.title)
+    let newTitle = main.appendChild(document.createElement("p"))
+    newTitle.textContent = film.opening_crawl
+}) */
+
+people.forEach(person => {
+    console.log(person.name)
+    let personImg = document.createElement('img')
+    personImg.src = "https://starwars-visualguide.com/assets/img/characters/10.jpg"
+    main.appendChild(personImg)
+})
