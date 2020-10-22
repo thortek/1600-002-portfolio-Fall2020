68a5abf83e628e67cd0f49fd70599ca733c1876b
Thor Anderson <ta.anderson@gmail.com>
2020-10-22 12:27:03 -0600

    Added for loop of posters

--- star-wars-api/main.js
+++ star-wars-api/main.js
@@ -1,19 +1,20 @@
 import { films } from './data/films.js'
 import { people } from './data/people.js'
 
-console.log(people.length)
+//console.log(people.length)
 
 const main = document.querySelector('main')
 
-/* films.forEach(film => {
-    console.log(film.title)
-    let newTitle = main.appendChild(document.createElement("p"))
-    newTitle.textContent = film.opening_crawl
-}) */
+for (let i = 0; i < 7; i++) {
+    let figure = document.createElement('figure')
+    let figImg = document.createElement('img')
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+    let figCaption = document.createElement('figcaption')
+    figCaption.textContent = films[i].title
 
-people.forEach(person => {
-    console.log(person.name)
-    let personImg = document.createElement('img')
-    personImg.src = "https://starwars-visualguide.com/assets/img/characters/10.jpg"
-    main.appendChild(personImg)
-})
+    figure.appendChild(figImg)
+    figure.appendChild(figCaption)
+
+    main.appendChild(figure)
+}
+  
