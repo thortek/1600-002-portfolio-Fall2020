0c78f225fb077bf45a7f7d4e5b4027e9eefa900a
Thor Anderson <ta.anderson@gmail.com>
2020-11-05 13:28:50 -0700

    Added a modal, refactored utils, added starfield

--- starships/main.js
+++ starships/main.js
@@ -1,8 +1,21 @@
 import { starships } from '../data/starships.js'
+import { removeChildren, getLastNumber } from '../utils/index.js'
 
 const nav = document.querySelector('.nav')
 const navList = document.querySelector('.navList')
 const shipView = document.querySelector('.main')
+
+const dialog = document.querySelector('.modal')
+const closeButton = document.querySelector('.modal-close')
+const modalBackground = document.querySelector('.modal-background')
+
+closeButton.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
+
+modalBackground.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
 
 function populateNav(starships) {
     starships.forEach(starship => {
@@ -24,7 +37,41 @@ function populateNav(starships) {
 }
 
 function populateShipView(shipData) {
-    console.log(shipData)
+    removeChildren(shipView)
+    let shipImage = document.createElement('img')
+    let shipNum = getLastNumber(shipData.url)
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+    shipImage.addEventListener('error', () => {
+        shipImage.hidden = true
+        dialog.classList.toggle("is-active")
+    }) // genius level 9001
+    shipView.appendChild(shipImage)
 }
 
 populateNav(starships)
+
+function addStarField(element, numStars) {
+    element.style.setProperty('background-color', 'black')
+    for (let i = 0; i < numStars; i++) {
+        let star = document.createElement('div')
+        star.style.setProperty('position', 'absolute')
+        star.style.setProperty('width', '2px')
+        star.style.setProperty('height', '2px')
+        star.style.setProperty('background-color', 'white')
+        let xy = getRandomPosition()
+        star.style.left = `${xy[0]}px`
+        star.style.top = `${xy[1]}px `
+        element.appendChild(star)
+    }
+}
+
+function getRandomPosition() {
+    let y = document.body.scrollHeight
+    let x = document.body.scrollWidth
+    let randomY = Math.floor(Math.random() * y)
+    let randomX = Math.floor(Math.random() * x)
+    return [randomX, randomY]
+}
+
+addStarField(document.querySelector('body'), 1000)
+
