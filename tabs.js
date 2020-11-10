dbf0d4eae9cc549a5d638109fd87cbed7326694f
Thor Anderson <ta.anderson@gmail.com>
2020-11-10 12:01:49 -0700

    Added Bulma tabs and iFrame

---
+++ tabs.js
@@ -0,0 +1,27 @@
+const charTab = document.querySelector('.characters')
+const filmsTab = document.querySelector('.films')
+const shipsTab = document.querySelector('.starships')
+
+const mainFrame = document.querySelector('.mainFrame')
+
+charTab.addEventListener('click', () => {
+    charTab.classList.add("is-active")
+    filmsTab.classList.remove("is-active")
+    shipsTab.classList.remove("is-active")
+    mainFrame.src = "./star-wars-characters/index.html"
+    //mainFrame.src = "https://www.uvu.edu/dgm"
+})
+
+filmsTab.addEventListener('click', () => {
+    filmsTab.classList.add("is-active")
+    shipsTab.classList.remove("is-active")
+    charTab.classList.remove("is-active")
+    mainFrame.src = "./star-wars-films/index.html"
+})
+
+shipsTab.addEventListener('click', () => {
+    shipsTab.classList.add("is-active")
+    filmsTab.classList.remove("is-active")
+    charTab.classList.remove("is-active")
+    mainFrame.src = "./starships/index.html"
+})
